005bab1f8ca29aafa3807ae05fdd968f1843e488
Albert Pimentel <albert.franca1992@gmail.com>
2024-10-31 11:09:30 -0300

    aqtualização dia 31/10/20024

--- contact/urls.py
+++ contact/urls.py
@@ -21,7 +21,20 @@ from contact import views
 app_name='contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('', views.index,name='index'),
+    path('search/', views.search, name='search'),
+    path('', views.login_view,name='login'),
+    path('index/', views.index,name='index'),
+
+    #crud-contact
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+    #user
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+
+
 
 ]
